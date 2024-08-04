8f65c3b6b2956425e81bd52137e01f392b514511
Abbby1007 <Abigailloken25@gmail.com>
2024-08-04 19:50:53 +0000

    Adding MOre Quesitons

--- twoScript.js
+++ twoScript.js
@@ -96,3 +96,143 @@ function twoq7()
     display.textContent = `Try Again`;
   }
 }
+
+function twoq8()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 16;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq9.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq9()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 18;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq10.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq10()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 20;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq11.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq11()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 22;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq12.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq12()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 24;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq13.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq13()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 26;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq14.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq14()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 28;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq15.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq15()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 30;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq16.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq16()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 32;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq17.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function twoq17()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 34;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="twoq18.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
