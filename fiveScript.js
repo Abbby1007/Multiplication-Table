fb48470790026f8fa325220a8445d0099ad2656c
Abbby1007 <Abigailloken25@gmail.com>
2024-08-05 18:32:35 +0000

    Add Questions 2 to 8 for Five

--- fiveScript.js
+++ fiveScript.js
@@ -14,3 +14,101 @@ function fiveq1()
     display.textContent = `Try Again`;
   }
 }
+
+// Question 2
+function fiveq2()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 10;
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="fiveq3.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+// Question 3
+function fiveq3()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 15;
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="fiveq4.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+// Question 4
+function fiveq4()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 20;
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="fiveq5.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+// Question 5
+function fiveq5()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 25;
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="fiveq6.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+// Question 6
+function fiveq6()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 30;
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="fiveq7.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+// Question 7
+function fiveq7()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 35;
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="fiveq8.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+// Question 8
+function fiveq8()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 40;
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="fiveq9.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
