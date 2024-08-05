fcaea1cb7959b3c5425998553c4db2257f1f0d7d
Abbby1007 <Abigailloken25@gmail.com>
2024-08-05 23:04:52 +0000

    Updating question 4 to 11 for six

--- sixScript.js
+++ sixScript.js
@@ -43,3 +43,115 @@ function sixq3()
     display.textContent = `Try Again`;
   }
 }
+
+function sixq4()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 24;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq5.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function sixq5()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 30;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq6.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function sixq6()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 36;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq7.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function sixq7()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 42;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq8.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function sixq8()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 48;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq9.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function sixq9()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 54;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq10.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function sixq10()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 60;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq11.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
+
+function sixq11()
+{
+  let input = document.getElementById('input').value;
+
+  let correctAnswer = 66;
+
+  if (correctAnswer == input) {
+    document.getElementById('appear').innerHTML = '<a href="sixq12.html"> <button> Next Question</button> </a>'
+  }
+  else {
+    display.textContent = `Try Again`;
+  }
+}
